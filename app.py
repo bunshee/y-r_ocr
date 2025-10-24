4f5dc90b6cd0f9c59055de066f59123070b2c7e3
Badr Gorchene <badrgorchene@gmail.com>
2025-10-24 08:26:34 +0100

    fix width

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ if uploaded_file is not None and api_key:
                         st.image(
                             table_data["image"],
                             caption=f"Rotated Image for Page {table_data['page_num']}",
-                            use_column_width="always",
+                            use_container_width=True,
                         )
     
                     # Display the raw text in an expander
